cbbe35294a47bef24abb11b52accb28e17535e54
mrshivrajsingh19 <mrshivrajsingh19@gmail.com>
2026-04-21 00:15:29 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAhp6vOXUcUIWJBlkQW1bs6G3iGqFRAkTA",
@@ -16,4 +15,3 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
